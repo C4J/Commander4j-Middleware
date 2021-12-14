0173f2e9cd80d3d69ffd6ff469bca385b3f68608
David Garratt <support@commander4j.com>
2021-12-14 13:18:39 +0000

    Log4j updated to version 2.15.0

--- b5Middleware/src/com/commander4j/mw/StartMain.java
+++ b5Middleware/src/com/commander4j/mw/StartMain.java
@@ -16,7 +16,7 @@ public class StartMain
 
 	Logger logger = org.apache.logging.log4j.LogManager.getLogger((StartMain.class));
 	public MiddlewareConfig cfg;
-	public static String version = "3.20";
+	public static String version = "3.21";
 	Boolean running = false;
 	LogArchiveThread archiveLog;
 	StatusThread statusthread;
