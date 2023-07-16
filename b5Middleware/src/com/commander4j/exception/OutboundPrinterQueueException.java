e9e848b4d2fe43fe46128e860df73312ebcbe2a7
David Garratt <support@commander4j.com>
2023-07-16 18:44:20 +0100

    Tidy

--- b5Middleware/src/com/commander4j/exception/OutboundPrinterQueueException.java
+++
@@ -1,9 +0,0 @@
-package com.commander4j.exception;
-
-public class OutboundPrinterQueueException extends Exception { 
-    private static final long serialVersionUID = 1L;
-
-	public OutboundPrinterQueueException(String errorMessage) {
-        super(errorMessage);
-    }
-}
