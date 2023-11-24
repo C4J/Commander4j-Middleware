7ad201188aa741947a1f21615ca93ddafc900906
David Garratt <support@commander4j.com>
2023-11-24 09:18:57 +0000

    Update to Saxon jars

--- b6Middleware/src/com/commander4j/mw/StartMain.java
+++ b6Middleware/src/com/commander4j/mw/StartMain.java
@@ -16,7 +16,7 @@ public class StartMain
 
 	Logger logger = org.apache.logging.log4j.LogManager.getLogger((StartMain.class));
 	public MiddlewareConfig cfg;
-	public static String version = "5.20";
+	public static String version = "5.21";
 	Boolean running = false;
 	LogArchiveThread archiveLog;
 	StatusThread statusthread;
