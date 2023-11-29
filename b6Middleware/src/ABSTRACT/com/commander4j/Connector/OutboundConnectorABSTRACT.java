4339e0f8da495eeaa0c235f7272ec730838ee837
David Garratt <support@commander4j.com>
2023-11-29 14:30:16 +0000

    Minor update

--- b6Middleware/src/ABSTRACT/com/commander4j/Connector/OutboundConnectorABSTRACT.java
+++ b6Middleware/src/ABSTRACT/com/commander4j/Connector/OutboundConnectorABSTRACT.java
@@ -57,66 +57,73 @@ public abstract class OutboundConnectorABSTRACT implements OutboundConnectorINTE
 		if (comparitor.equals("") == false)
 		{
 			// We need to do a comparison
-			System.out.println("Comparitor =" + comparitor);
+			logger.debug("Comparitor =" + comparitor);
 
 			JXMLDocument document = new JXMLDocument();
-			document.setDocument(getData());
+			document.setDocument(data);
+			logger.debug(util.getStringFromDocument(data));
+			
+			//document.setDocument(getData());
 
 			// Determine parm1 value
-			String parm1value = "";
+			String parm1value = getOutboundInterface().getCompareParam1();
 			String parm1type = getOutboundInterface().getCompareParam1_Type();
-			System.out.println("parm1type =" + parm1type);
 
+			String parm2value =getOutboundInterface().getCompareParam2();
+			String parm2type = getOutboundInterface().getCompareParam2_Type();
+
+			
 			if (parm1type.equals("xquery"))
 			{
-				parm1value = util.replaceNullStringwithBlank(document.findXPath(getOutboundInterface().getCompareParam1()).trim());
+				parm1value = document.findXPath(parm1value);
 
 			}
-
-			if (parm1type.equals("literal"))
-			{
-				parm1value = getOutboundInterface().getCompareParam1();
-			}
-			System.out.println("parm1value =" + parm1value);
-
-			// Determine parm2 value
-			String parm2value = "";
-			String parm2type = getOutboundInterface().getCompareParam2_Type();
-			System.out.println("parm2type =" + parm2type);
 
 			if (parm2type.equals("xquery"))
 			{
-				parm2value = util.replaceNullStringwithBlank(document.findXPath(getOutboundInterface().getCompareParam2()).trim());
+				parm2value = util.replaceNullStringwithBlank(document.findXPath(parm2value));
 
 			}
 
-			if (parm2type.equals("literal"))
-			{
-				parm2value = getOutboundInterface().getCompareParam2();
-			}
-			System.out.println("parm2value =" + parm2value);
+			logger.debug("parm1type 	=" + parm1type);
+			logger.debug("parm1value 	=" + parm1value);
 
-			if (comparitor.equals("NOT EQUAL"))
+			logger.debug("parm2type 	=" + parm2type);
+			logger.debug("parm2value 	=" + parm2value);
+
+			logger.debug("comparitor 	=" + comparitor);
+			
+			logger.debug("Is [" + parm1value + "] "+comparitor+" to [" + parm2value + "]");
+			
+			if (comparitor.equals("EQUAL"))
 			{
-				logger.debug("Comparing parm1value [" + parm1value + "] with parm2value [" + parm2value + "]");
-				if (parm1value.equals(parm2value))
+				if (parm1value.equals(parm2value) == true)
 				{
-					System.out.println("SAME");
-					saveFile = false;
+					logger.debug("SAME - PASS");
+					saveFile = true;
 				}
 				else
 				{
-					System.out.println("DIFFERENT");
-				}
-			}
-
-			if (comparitor.equals("EQUAL"))
-			{
-				if (parm1value.equals(parm2value) == false)
-				{
+					logger.debug("DIFFERENT - FAIL");
 					saveFile = false;
 				}
 			}
+
+			if (comparitor.equals("NOT EQUAL"))
+			{				
+				if (parm1value.equals(parm2value) == false)
+				{
+
+					logger.debug("DIFFERENT - PASS");
+					saveFile = true;
+				}
+				else
+				{
+					logger.debug("SAME - FAIL");
+					saveFile = false;
+				}
+			}
+
 		}
 
 		if (saveFile == true)
--- b6Middleware/xslt/DMO_PRODDEC_XML_C4J.xsl
+++ b6Middleware/xslt/DMO_PRODDEC_XML_C4J.xsl
@@ -1,3 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
 	xmlns:xs="http://www.w3.org/2001/XMLSchema"
 	xmlns:c4j="http://www.commander4j.com"
